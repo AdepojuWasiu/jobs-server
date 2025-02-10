23def1728eb594ad1ef835306a074114eb37343b
AdepojuWasiu <adepojuwasiu031@gmail.com>
2025-02-10 05:24:12 +0100

    Page update

--- config/db.js
+++ config/db.js
@@ -14,6 +14,35 @@ const supabase = createClient(supabaseUrl, supabaseKey);
     console.error('Supabase connection failed: ', error.message);
   } else {
     console.log('✅ Supabase connected successfully!');
+    console.log('To test the server you can use POSTMAN');
+    console.log(`To register user send post request url: https://jobs-red-ten.vercel.app/api/auth/register  body: {
+                     "name": "Your name",
+                    "email": "youremail@gmail.com",
+                      "password": "your passwoed"
+                    }`
+                  
+                  );
+
+     console.log(`To login user send post request url: https://jobs-red-ten.vercel.app/api/auth/login body: {
+                     "email": "youremail@gmail.com",
+                       "password": "your passwoed"
+                     }
+                       
+                     `);
+
+    console.log(`To register jobs send post request url: https://jobs-red-ten.vercel.app/api/jobs body: {
+
+            "title": "job title",
+            "company": "Company name",
+            "location": "Location of job",
+            "requirements": "requiremwnts for the job"
+        }
+            
+        `);
+     console.log(`To fetch all jobs send get request url: https://jobs-red-ten.vercel.app/api/jobs body:none
+          
+      `);
+     
   }
 
 })();
